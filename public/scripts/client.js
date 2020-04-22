ef085213c1331a78c2049e3576e073f4b60264bb
Jessie Wright <jessie.wright@gmail.com>
2020-04-22 19:32:24 +0000

    put ajax handleron new tweet form

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -25,40 +25,15 @@ $(document).ready( function () {
 
   const renderTweets = function(tweets) {
     let $container = $('#tweets-container');
-
     for(const tweet of tweets) {
       $container.append(createTweetElement(tweet));
     }
-
-
   }
+
+  $('.new-tweet form').submit(function(event) {
+    event.preventDefault();
+    $.post('/tweets/', $(this).serialize())
+    .then(res => console.log('promise resolved', res.status));
+  })
   
-
-
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-  renderTweets(data);
 })
