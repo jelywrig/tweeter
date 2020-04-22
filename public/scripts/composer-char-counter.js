5b41d2026e3aad47d2604d08ae0c1b79fcace8eb
Jessie Wright <jessie.wright@gmail.com>
2020-04-22 01:00:36 +0000

    added  tweet content styling

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,14 @@
 $(document).ready( () => {
   const $input = $('#tweet-text');
   const $charCounter = $('output[for="tweet-text"]');
-  ;
+  
   // use keyup so that it recounts after backspace
   $input.keyup(function(event) {
     $charCounter.val(140 - this.value.length);
     if($charCounter.val() < 0) {
-      $charCounter.css('color', 'red');
+      $charCounter.addClass('over');
     } else {
-      $charCounter.css('color', '#9b9b9b');
+      $charCounter.removeClass('over');
     }
   });
 })
