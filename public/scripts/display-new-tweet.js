59ba6040194af858853c7b934275383201d2f120
Jessie Wright <jessie.wright@gmail.com>
2020-04-23 16:33:11 +0000

    added simple animation

--- public/scripts/display-new-tweet.js
+++ public/scripts/display-new-tweet.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
 
+
+
   $('#new-tweet-button').click(function(event) {
     event.preventDefault();
+    
+
     if($('section.new-tweet').first().is(':hidden')) {
       $('section.new-tweet').slideDown();
+      $('#arrows').animate({height:"+=25"},500);
     } else {
       $('section.new-tweet').slideUp();
+      $('#arrows').animate({height:"-=25"},500);
     }
     
 
