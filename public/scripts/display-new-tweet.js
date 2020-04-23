eb0dbe0d9b621a2d5af0fc40f3f8b7f9e0b581de
Jessie Wright <jessie.wright@gmail.com>
2020-04-23 20:08:11 +0000

    switched to css keyframe animation

--- public/scripts/display-new-tweet.js
+++ public/scripts/display-new-tweet.js
@@ -1,20 +1,11 @@
 $(document).ready(function() {
 
-const onAnimationCompletion = function () {
-  $('#arrows').animate({top:"-=10"},200);
-}
-
   $('#new-tweet-button').click(function(event) {
     event.preventDefault();
-    
-
     if($('section.new-tweet').first().is(':hidden')) {
       $('section.new-tweet').slideDown();
-      $('#arrows').animate({top:"+=10"},200, onAnimationCompletion);
     } else {
       $('section.new-tweet').slideUp();
-      $('#arrows').animate({top:"+=10"},200, onAnimationCompletion);
-      
     }
     
 
